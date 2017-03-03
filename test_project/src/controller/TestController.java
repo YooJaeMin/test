7ae045f552238da18e4254a1259a2d430097d429
admin <admin@192.168.10.3>
2017-03-03 18:54:05 +0900

    real

--- test_project/src/controller/TestController.java
+++ test_project/src/controller/TestController.java
@@ -4,8 +4,6 @@ package controller;
 import java.util.List;
 import java.util.Map;
 
-//github.com/YooJaeMin/test.git
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
