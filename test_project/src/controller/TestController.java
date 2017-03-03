9a1d4260ba07230317afa160674b801512e84ce3
admin <admin@192.168.10.3>
2017-03-03 18:46:16 +0900

    controller 수정 service 수정 fish-mapper수정

--- test_project/src/controller/TestController.java
+++ test_project/src/controller/TestController.java
@@ -1,6 +1,8 @@
 package controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,28 @@ public class TestController {
 	@Autowired
 	TestService ts;
 	
-	@RequestMapping("/beer/list")
+	@RequestMapping("/beer/result")
 	public ModelAndView listHandle(){
-		ModelAndView mav = new ModelAndView();
+		List allList = ts.getAll();
+		List alchor  = ts.getalchorList();
+		List ipa = ts.getStyleList("IPA");
+		List stout = ts.getStyleList("STOUT");
+		List pilsner = ts.getStyleList("PILSNER");
+		List ale = ts.getStyleList("ALE");
+		List score = ts.getScoreList();
+		
+		ModelAndView mav = new ModelAndView("/beer/result");
+		mav.addObject("allList", allList);
+		mav.addObject("alchorList", alchor);
+		mav.addObject("styleListI", ipa);
+		mav.addObject("styleListS", stout);
+		mav.addObject("styleListP", pilsner);
+		mav.addObject("styleListA", ale);
+		mav.addObject("scoreList", score);
+		
 		return mav;
 	}
+	
 	
 	@RequestMapping("/beer/insert")
 	public ModelAndView insertHandle(){
