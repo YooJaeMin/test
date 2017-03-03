10d8d913ff2abc9656c5df179caf7737733c75bd
admin <admin@192.168.10.2>
2017-03-03 18:17:33 +0900

    add

--- test_project/src/controller/TestController.java
+++ test_project/src/controller/TestController.java
@@ -1,9 +1,12 @@
 package controller;
 
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.TestService;
@@ -15,6 +18,12 @@ public class TestController {
 	
 	@RequestMapping("/beer/list")
 	public ModelAndView listHandle(){
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
+	
+	@RequestMapping("/beer/search")
+	public ModelAndView searchHandle(@RequestParam Map map){
 		ModelAndView mav = new ModelAndView();
 		return mav;
 	}
